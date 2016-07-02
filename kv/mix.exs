33596e1ee7042df023f2f08b0f4d47963e4a49ec
Nicolas Kovalsky <kovalsky.nicolas@gmail.com>
2016-07-02 15:07:17 -0300

    avanzo en cliente en modo actores, cambia de server

--- kv/mix.exs
+++ kv/mix.exs
@@ -14,7 +14,7 @@ defmodule KV.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:httpotion, :logger, :cowboy, :plug]]
+    [applications: [:logger, :cowboy, :plug]]
   end
 
   # Dependencies can be Hex packages:
@@ -28,7 +28,6 @@ defmodule KV.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:cowboy, "~> 1.0.0"},
-     {:plug, "~> 1.0"},
-    {:httpotion, "~> 3.0.0"}]
+     {:plug, "~> 1.0"}]
   end
 end
